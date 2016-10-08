a19aff0d11812ad300062c529f0a4a2cfdb1bfe4
ar664 <ar664@njit.edu>
2016-10-08 17:14:12 -0400

    Small Update, commenting and coding the rest of window + element. Window now inherits element, and so will the rest of the defined elements.

--- include/ui.h
+++ include/ui.h
@@ -14,6 +14,8 @@
  * @var		mName		The name of the element
  * @var		mPosition	The position of the element relative to its
  * 					parent class.
+ * @var		mSize		The size of the element as a percentage of 
+ * 					the parent window/element.
  * @var		mGrapics	The sprite info, if any.
  *   
  *
@@ -21,37 +23,45 @@
 virtual class Element
 {
 public:
+	Element(Element* childs, std::String name, float pos[2], float size[2], std::String image)
+	{
+		mChildren = childs;
+		mName = name;
+		mPosition = pos;
+		mSize = size;
+		//Load image
+	}
 	Element*	mChildren;
 	std::String	mName;
 	float		mPosition[2];
 	float		mSize[2];
-	Sprite		mGraphics;
-	virtual void	Update();
-}
+	Sprite*		mGraphics;
+	//Empty Declared Voids
+	virtual void	Update(){};
+	virtual void	Draw(){};
+};
 
 
 /* @name	Window
  * 
  * @breif	These hold elements, and determine their positions based 
- * 		on the area it defines.
+ * 		on the area it defines. This is main holder for all elements
+ * 		as it does the updating and drawing of all children.
  * 
- * @var		mChildren	Elements inside this window.
- * @var		mName		Name of the window.
- * @var		mPosition	Position of the window in relation to either
- *					screen or parent windows.
+ * @fn		Update		Iterates over children to update position
+ * 				and states.
+ * @fn		Draw		Iterates over children to draw windows
+ * 				based on focus and position.
  *
  */
-class Window
+class Window : public Element
 {
 public:
-	Window();
-	Element*	mChildren;
-	std::String	mName;
-	float		mPosition[2];
+	Window(Element* childs, std::String name, float pos[2], float size[2], std::String image);
 	bool		mFocus;
 	void		Update();
 	void		Draw();
-}
+};
 
 #endif
 
